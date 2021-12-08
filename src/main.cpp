5b82cc63b3943dcae9ba6b80b1e6a1f9a98abd36
gratisCobalt <jaywatson@gmx.net>
2021-12-08 15:55:22 +0100

    included MQTT but not implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,39 @@
 #include <Adafruit_NeoPixel.h>
 
-// LED stripe stuff
+// ============== LED stripe ==============
 // #include <LEDstripe.h> (already included by <connectWIFI.h>)
 
-// connectWIFI stuff
+
+// ============== MQTT ==============
+#include <MQTT/client.h>
+
+
+// ============== connectWIFI ==============
 #include <connectWIFI.h>
 # include <credentials.h>
 
-// FAN stuff
+
+// ============== FAN ==============
 #include <fan.h>
 
-// Relais stuff
+
+// ============== RELAIS ==============
 #include <relais.h>
+
 
 void setup()
 {
     // ============== general ==============
     Serial.begin(9600);
 
+
     // ============== fan ==============
     fan_setup();
 
+
     // ============== LEDstripe ==============
     LEDstripe_setup();
+
 
     // ============== relais ==============
     relais_setup();
@@ -33,15 +44,17 @@ void loop()
     // ============== connectWIFI ==============
     // connectWIFI(ssid, password);
 
+
     // ============== relais ==============
     // relaisON();
     // relaisOFF();
 
+
     // ============== fan ============== 
     fan_loop();
 
+
     // ============== LEDstripe ============== 
     // LEDstripe_loop(); // takes very long
-
     // applyAllLeds(0, 255, 255);
 }
