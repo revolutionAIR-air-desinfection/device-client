e63052fc134b3d33da814ec8e5846366ef842859
gratisCobalt <jaywatson@gmx.net>
2022-04-10 11:22:01 +0200

    added MQTT command support

--- src/WiFi/ClientHandler.h
+++ src/WiFi/ClientHandler.h
@@ -1,36 +1,130 @@
-#include <UVC/relais.h>
-#include <fan/fan.h>
+#include "../UVC/relais.h"
+#include "../fan/fan.h"
 
 bool isDeviceON = false;
 
-void setDeviceState(bool state) {
+void setDeviceState(bool state)
+{
     isDeviceON = state;
 }
 
-void handleCommand(String cmd) {
-
+void debug(String cmd)
+{
     Serial.println(cmd);
+    
+}
 
-    if(cmd.equalsIgnoreCase("FAN ON")) {
+void fanspeed(String cmd)
+{
+    if (cmd == "ON")
+    {
         fanON();
-
-    } else if(cmd.equalsIgnoreCase("FAN OFF")) {
+        Serial.println("fan ON");
+    } 
+    else if (cmd == "OFF") 
+    {
         fanOFF();
-
-    } else if(cmd.equalsIgnoreCase("UVC ON")) {
-        relaisON();
-
-    } else if(cmd.equalsIgnoreCase("UVC OFF")) {
-        relaisOFF();
-
-    } else if(cmd.equalsIgnoreCase("DEVICE ON")) {
-        setDeviceState(true);
-        
-    } else if(cmd.equalsIgnoreCase("DEVICE OFF")) {
-        setDeviceState(false);
-        
-    } else {
-        // TODO: not implemented yet
+        Serial.println("fan OFF");
+    } 
+    else 
+    {
+        Serial.println("ERROR: unknown FAN value: " + cmd);
     }
+}
 
+void uvc(String cmd)
+{
+    if (cmd == "ON")
+    {
+        relaisON();
+        Serial.println("uvc ON");
+    } 
+    else if (cmd == "OFF") 
+    {
+        relaisOFF();
+        Serial.println("uvc OFF");
+    } 
+    else 
+    {
+        Serial.println("ERROR: unknown RELAIS value: " + cmd);
+    }
+}
+
+void power(String cmd)
+{
+    if (cmd == "ON")
+    {
+        setDeviceState(true);
+        Serial.println("device ON");
+    }
+    else if (cmd == "OFF") 
+    {
+        setDeviceState(false);
+        Serial.println("device OFF");
+    } 
+    else 
+    {
+        Serial.println("ERROR: unknown RELAIS value: " + cmd);
+    }
+}
+
+void autopilot(String cmd)
+{
+    if (cmd == "ON")
+    {
+        relaisON();
+        fanON();
+        // TODO: apply status color
+
+        Serial.println("auto ON");
+        Serial.println("[AUTO] uvc ON");
+        Serial.println("[AUTO] fan ON");
+    } 
+    else if (cmd == "OFF") 
+    {
+        // do nothing
+        Serial.println("auto OFF");
+    } 
+    else 
+    {
+        Serial.println("ERROR: unknown AUTO value: " + cmd);
+    }
+}
+
+void rgb(String cmd)
+{
+    // TODO: not implemented yet
+    Serial.println("ERROR: UNIMPLEMENTED METHOD");
+}
+
+void handleCommand(String topic, String cmd)
+{
+    if (topic == "debug")
+    {
+        debug(cmd);
+    }
+    else if (topic == "fanspeed")
+    {
+        fanspeed(cmd);
+    }
+    else if (topic == "uvc")
+    {
+        uvc(cmd);
+    }
+    else if (topic == "power")
+    {
+        power(cmd);
+    }
+    else if (topic == "auto")
+    {
+        autopilot(cmd);
+    }
+    else if (topic == "rgb")
+    {
+        rgb(cmd);
+    }
+    else 
+    {
+        // do nothing
+    }
 }
