c406368e34d5d9ea971493f8ef90cc516caf0667
gratisCobalt <jaywatson@gmx.net>
2022-04-10 11:22:55 +0200

    main moved do "revolutionAIR.ino"

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,72 @@
-#include <Adafruit_NeoPixel.h>
+// #include <Adafruit_NeoPixel.h>
 
-// ============== LED stripe ==============
-// #include <LED/LEDstripe.h> // (already included by <connectWIFI.h>)
+// // ============== LED stripe ==============
+// // #include <LED/LEDstripe.h> // (already included by <connectWIFI.h>)
 
-// ============== WIFI ==============
-#include <WiFi/connectWIFI.h>
-
-
-// ============== MQTT ==============
-#include <MQTT/client.h>
+// // ============== WIFI ==============
+// // #include <WiFi/connectWIFI.h>
 
 
-// ============== connectWIFI ==============
-// # include <credentials/credentials.h> // (already included by <connectWIFI.h>)
+// // ============== MQTT ==============
+// #include <MQTT/mqttclient.h>
 
 
-// ============== FAN ==============
-// #include <fan/fan.h> // (already included by <ClientHandler.h>)
+// // ============== connectWIFI ==============
+// // # include <credentials/credentials.h> // (already included by <connectWIFI.h>)
 
 
-// ============== RELAIS ==============
-// #include <UVC/relais.h>  // (already included by <ClientHandler.h>)
+// // ============== FAN ==============
+// // #include <fan/fan.h> // (already included by <ClientHandler.h>)
+
+
+// // ============== RELAIS ==============
+// // #include <UVC/relais.h>  // (already included by <ClientHandler.h>)
 
 
 
-void setup()
-{
-    // ============== general ==============
-    Serial.begin(9600);
+// void setup()
+// {
+//     // ============== general ==============
+//     Serial.begin(9600);
 
 
-    // ============== fan ==============
-    fan_setup();
+//     // ============== fan ==============
+//     // fan_setup();
 
 
-    // ============== LEDstripe ==============
-    LEDstripe_setup();
+//     // ============== LEDstripe ==============
+//     // LEDstripe_setup();
 
 
-    // ============== relais ==============
-    relais_setup();
+//     // ============== relais ==============
+//     // relais_setup();
 
 
-    // ============== mqtt ==============
-    mqtt_setup();    
-}
- 
-void loop()
-{
-
-    // ============== WIFI ==============
-    connectWIFI();
-    clientLoop();
-
-    // ============== relais ==============
-    // relaisON();
-    // relaisOFF();
-
-    // ============== fan ============== 
-    // fan_loop(50);
-
-
-    // ============== mqtt ==============
-    mqtt_loop();
-
-
-    // ============== LEDstripe ============== 
-    LEDstripe_loop(); // takes very long
+//     // ============== mqtt ==============
     
-}
+// }
+ 
+// void loop()
+// {
+
+//     // ============== WIFI ==============
+//     connectWIFI();
+//     // clientLoop();
+
+//     // ============== relais ==============
+//     // relaisON();
+//     // relaisOFF();
+
+//     // ============== fan ============== 
+//     // fan_loop(50);
+
+
+//     // ============== mqtt ==============
+//     mqtt_setup();
+//     mqtt_loop();
+
+
+//     // ============== LEDstripe ============== 
+//     // LEDstripe_loop(); // takes very long
+    
+// }
