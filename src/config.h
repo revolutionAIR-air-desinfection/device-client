a2ea0c8c17e257c8f6faa9ad2fb010a8f6899766
gratisCobalt <jaywatson@gmx.net>
2022-05-21 15:59:49 +0200

    global variables

---
+++ src/config.h
@@ -0,0 +1,17 @@
+/*
+    DEFINITIONS FOR PINS
+*/
+
+#define TEMPERATURE D0          // Temperature sensor
+#define RELAIS D1               // Relais for UVC LEDs
+#define LED D2                  // LED Stripe
+#define RPM 00                  // FAN RPM
+#define PWM D7                  // FAN PWM
+
+/*
+    Settings for LED Stripe
+*/
+#define NUMPIXELS 34            // number of leds on LED stripe 
+#define LEDfade_duration 10000  // time in ms the LED stripe needs to fade for 1 cycle
+#define pulseDelay 10           // delay for pulsing warnging/error LED indicator
+
