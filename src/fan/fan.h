d2d194713fd0248a96f5ee4b5edcca75543e1b5d
gratisCobalt <jaywatson@gmx.net>
2022-05-21 16:01:40 +0200

    added PWM support

--- src/fan/fan.h
+++ src/fan/fan.h
@@ -1,25 +1,39 @@
-#define PWM D7
+//! UNABLE TO CONTROL FANS DUE TO MISSING PWM SUPPORT
+#include <Arduino.h>
+
+float speed_percentage; // percentage of fan speed varies from 0 to 100
 
 void fan_setup()
 {
     pinMode(PWM, OUTPUT);
 }
 
-/*
-void fan_loop(float percentage)
+void fan_loop()
 {
     // PWM Value varries from 0 to 1023
-    // analogWrite(PWM, 1024 * (percentage / 100));
-    // delay(2000);
-}
-*/
-
-void fanON() {
-    // analogWrite(PWM, 1023);
+    analogWrite(PWM, 1023 * (speed_percentage / 100));
+    delay(2000);
 }
 
-void fanOFF() {
-    // analogWrite(PWM, 0);
+void setFanSpeed(float percentage)
+{
+    percentage = speed_percentage; //! NEW FOR PWM
 }
 
-// TODO: hardware unable to control fans
+unsigned int getRPM()
+{
+    // TODO untested method
+    return analogRead(RPM); //! NEW FOR PWM
+}
+
+void fanON() // TODO deprecated
+{
+    speed_percentage = 100; //! NEW FOR PWM
+    analogWrite(PWM, 1023);
+}
+
+void fanOFF() // TODO deprecated
+{
+    speed_percentage = 0; //! NEW FOR PWM
+    analogWrite(PWM, 0);
+}
