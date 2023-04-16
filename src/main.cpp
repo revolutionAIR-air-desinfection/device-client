5ceaa80085fc64edcd98e6d0ed462d01e002c7eb
domi <44619866+gratisCobalt@users.noreply.github.com>
2023-04-16 21:31:39 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -37,3 +37,40 @@
 // {
 //     controller.run();
 // }
+
+
+#include <Arduino.h>
+// #include <Thread.h>
+// #include <ThreadController.h>
+
+#include "./MQTT/mqttclient.h"
+
+// ThreadController controller = ThreadController();
+
+// Thread *mqttThread = new Thread();
+// Thread *rainbowThread = new Thread();
+
+void setup()
+{
+    Serial.begin(9600);
+    // srand (static_cast <unsigned> (time(0))); // seeding random number generator
+
+    ledStripe_setup();
+    // relais_setup();
+    // fan_setup();
+    // temperature_setup();
+    // mqtt_setup();
+
+    // // configuring threads
+    // mqttThread->onRun(mqtt_loop);
+    // rainbowThread->onRun(ledStripe_loop);
+
+    // controller.add(mqttThread);
+    // controller.add(rainbowThread);
+}
+
+void loop()
+{
+    // controller.run();
+    ledStripe_loop();
+}
