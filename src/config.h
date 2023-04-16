9170da75da332d5f8d19fbb55e32ffbc42ab102e
domi <44619866+gratisCobalt@users.noreply.github.com>
2023-04-16 21:31:19 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -6,21 +6,25 @@
     DEFINITIONS FOR PINS
 */
 
-#define TEMPERATURE D0 // Temperature sensor
-#define RELAIS D1      // Relais for UVC LEDs
-#define LED D2         // LED Stripe
+#define TEMPERATURE D6 // Temperature sensor
+#define RELAIS D3      // Relais for UVC LEDs
+#define LED D1         // LED Stripe outside
+#define LED_IN D2      // LED Stripe inside
 #define PWM D4         // FAN PWM
 #define PWM_RELAIS D5  // transistor to turn off the fan
 
 /*
     Settings for LED Stripe
 */
-#define NUMPIXELS 34           // number of leds on LED stripe
-#define pulseDelay 10          // delay for pulsing warnging/error LED indicator
-#define LEDfade_duration 10000 // time in ms the LED stripe needs to fade for 1 cycle
-#define LEDstep_delay 500      // time for "rainbowMoving" to wait until stepping 1 LED further
+#define NUMPIXELS 34            // number of leds on LED stripe
+#define pulseDelay 15          // delay for pulsing warnging/error LED indicator
+#define LEDfade_duration 10000  // time in ms the LED stripe needs to fade for 1 cycle
+#define LEDstep_delay 500       // time for "rainbowMoving" to wait until stepping 1 LED further
+#define maxBrightness 0.8        // 0.1 - 1.0 Value for Brightness
+#define minBrightness 0.15
 
 Adafruit_NeoPixel pixels(NUMPIXELS, LED, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels_in(NUMPIXELS, LED_IN, NEO_GRB + NEO_KHZ800);
 
 /*
     global colors
