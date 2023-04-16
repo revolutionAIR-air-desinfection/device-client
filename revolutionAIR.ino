48a08dfa42adae05d80ea1d0ec8f4de31fbba68e
domi <44619866+gratisCobalt@users.noreply.github.com>
2023-04-16 21:30:56 +0200

    Update revolutionAIR.ino

--- revolutionAIR.ino
+++ revolutionAIR.ino
@@ -1,4 +1,45 @@
-#include <Arduino.h>
+// // // /*
+// //     FILE ONLY EXISTS TO BE ABLE TO COMPILE IN VS CODE WITH PLATFORMIO
+// //     CODE INSIDE THIS FILE IS NOT USED
+// // */
+
+// // #include <Arduino.h>
+// // #include <Thread.h>
+// // #include <ThreadController.h>
+
+// // #include "./MQTT/mqttclient.h"
+
+// // ThreadController controller = ThreadController();
+
+// // Thread *mqttThread = new Thread();
+// // Thread *rainbowThread = new Thread();
+
+// // void setup()
+// // {
+// //     Serial.begin(9600);
+// //     srand (static_cast <unsigned> (time(0))); // seeding random number generator
+
+// //     ledStripe_setup();
+// //     relais_setup();
+// //     fan_setup();
+// //     temperature_setup();
+// //     mqtt_setup();
+
+// //     // configuring threads
+// //     mqttThread->onRun(mqtt_loop);
+// //     rainbowThread->onRun(ledStripe_loop);
+
+// //     controller.add(mqttThread);
+// //     controller.add(rainbowThread);
+// // }
+
+// // void loop()
+// // {
+// //     controller.run();
+// // }
+
+
+// #include <Arduino.h>
 // #include <Thread.h>
 // #include <ThreadController.h>
 
@@ -9,27 +50,27 @@
 // Thread *mqttThread = new Thread();
 // Thread *rainbowThread = new Thread();
 
-void setup()
-{
-    Serial.begin(9600);
-    // srand (static_cast <unsigned> (time(0))); // seeding random number generator
+// void setup()
+// {
+//     Serial.begin(9600);
+//     srand (static_cast <unsigned> (time(0))); // seeding random number generator
 
-    // ledStripe_setup();
-    // relais_setup();
-    // fan_setup();
-    // temperature_setup();
-    // mqtt_setup();
+//     ledStripe_setup();
+//     relais_setup();
+//     fan_setup();
+//     temperature_setup();
+//     mqtt_setup();
 
-    // // configuring threads
-    // mqttThread->onRun(mqtt_loop);
-    // rainbowThread->onRun(ledStripe_loop);
+//     // configuring threads
+//     mqttThread->onRun(mqtt_loop);
+//     rainbowThread->onRun(ledStripe_loop);
 
-    // controller.add(mqttThread);
-    // controller.add(rainbowThread);
-}
+//     controller.add(mqttThread);
+//     controller.add(rainbowThread);
+// }
 
-void loop()
-{
-    // controller.run();
-    Serial.println("dd");
-}
+// void loop()
+// {
+//     // controller.run();
+//     Serial.println("ww");
+// }
